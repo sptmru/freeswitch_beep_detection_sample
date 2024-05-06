8242de2a9ff1064aeceb687992ed94ea951f3d9e
Soslan Aldatov <soslanaldatov@gmail.com>
2024-05-06 11:56:27 +0400

    add play_beep_tone extension to public dialplan

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -2,7 +2,7 @@
 
 sed -i '/<X-PRE-PROCESS cmd="set" data="default_password=[^"]*"\/>/c\<X-PRE-PROCESS cmd="set" data="default_password='"${EXTENSION_PASSWORD:-extensionpassword}"'"\/>' /usr/local/freeswitch/conf/vars.xml
 
-sed -i '/<context name="default">/a \
+sed -i '/<context name="public">/a \
 <extension name="play_beep_tone">\n\
       <condition field="destination_number" expression="^74331$">\n\
         <action application="avmd_start" data="inbound_channel=1,outbound_channel=1,debug=0,report_status=1,fast_math=1,require_continuous_streak=0,require_continuous_streak_amp=0,simplified_estimation=1,detection_mode=1"/>\n\
@@ -11,6 +11,6 @@ sed -i '/<context name="default">/a \
         <action application="avmd_stop"/>\n\
         <action application="hangup"/>\n\
       </condition>\n\
-    </extension>' /usr/local/freeswitch/conf/dialplan/default.xml
+    </extension>' /usr/local/freeswitch/conf/dialplan/public.xml
 
 /usr/local/freeswitch/bin/freeswitch -nonat -nf -nc
